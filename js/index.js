b99847b36ad39c85a376552f1b74173422c1b5ef
SamTush <samo4421@icloud.com>
2023-02-22 10:37:49 +0300

    refactor to use javascript classes

--- js/index.js
+++ js/index.js
@@ -1,56 +1,60 @@
+class Book {
+  constructor(id, title, author, isComplete = false) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.isComplete = isComplete;
+  }
+
+  createEl() {
+    const bookEl = document.createElement('li');
+    bookEl.setAttribute('id', this.id);
+
+    const titleEl = document.createElement('p');
+    titleEl.textContent = this.title;
+    bookEl.appendChild(titleEl);
+
+    const authorEl = document.createElement('p');
+    authorEl.textContent = this.author;
+    bookEl.appendChild(authorEl);
+
+    const deleteButtonEl = document.createElement('button');
+    deleteButtonEl.classList.add('remove-book');
+    deleteButtonEl.classList.add('btn');
+    deleteButtonEl.classList.add('btn-outline-dark');
+    deleteButtonEl.textContent = 'Remove';
+    deleteButtonEl.setAttribute('data-book-id', this.id);
+    bookEl.appendChild(deleteButtonEl);
+
+    const hr = document.createElement('hr');
+    bookEl.appendChild(hr);
+
+    booksSection.appendChild(bookEl);
+  }
+
+  static addBook(title, author) {
+    const book = new Book(new Date().getTime(), title, author);
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+    book.createEl();
+  }
+
+  static removeBook(bookId) {
+    books = books.filter((book) => book.id !== parseInt(bookId, 10));
+    localStorage.setItem('books', JSON.stringify(books));
+    const bookEl = document.getElementById(bookId);
+    bookEl.remove();
+  }
+}
+
 const booksSection = document.querySelector('.books');
 const titleInput = document.querySelector('#title');
 const authorInput = document.querySelector('#author');
 
 let books = JSON.parse(localStorage.getItem('books')) || [];
 
-function createBook(book) {
-  const bookEl = document.createElement('li');
-  bookEl.setAttribute('id', book.id);
-
-  const titleEl = document.createElement('p');
-  titleEl.textContent = book.title;
-  bookEl.appendChild(titleEl);
-
-  const authorEl = document.createElement('p');
-  authorEl.textContent = book.author;
-  bookEl.appendChild(authorEl);
-
-  const deleteButtonEl = document.createElement('button');
-  deleteButtonEl.classList.add('remove-book');
-  deleteButtonEl.classList.add('btn');
-  deleteButtonEl.classList.add('btn-outline-dark');
-  deleteButtonEl.textContent = 'Remove';
-  deleteButtonEl.setAttribute('data-book-id', book.id);
-  bookEl.appendChild(deleteButtonEl);
-
-  const hr = document.createElement('hr');
-  bookEl.appendChild(hr);
-
-  booksSection.appendChild(bookEl);
-}
-
 if (localStorage.getItem('books')) {
-  books.forEach((book) => createBook(book));
-}
-
-function addBook(title, author) {
-  const book = {
-    id: new Date().getTime(),
-    title,
-    author,
-    isComplete: false,
-  };
-  books.push(book);
-  localStorage.setItem('books', JSON.stringify(books));
-  createBook(book);
-}
-
-function removeBook(bookId) {
-  books = books.filter((book) => book.id !== parseInt(bookId, 10));
-  localStorage.setItem('books', JSON.stringify(books));
-  const bookEl = document.getElementById(bookId);
-  bookEl.remove();
+  books.forEach((book) => new Book(book.id, book.title, book.author, book.isComplete).createEl());
 }
 
 document.querySelector('#book-form').addEventListener('submit', (event) => {
@@ -58,7 +62,7 @@ document.querySelector('#book-form').addEventListener('submit', (event) => {
   const title = titleInput.value;
   const author = authorInput.value;
   if (title !== '' && author !== '') {
-    addBook(title, author);
+    Book.addBook(title, author);
     titleInput.value = '';
     authorInput.value = '';
     titleInput.focus();
@@ -68,6 +72,6 @@ document.querySelector('#book-form').addEventListener('submit', (event) => {
 booksSection.addEventListener('click', (event) => {
   if (event.target.classList.contains('remove-book')) {
     const bookId = event.target.getAttribute('data-book-id');
-    removeBook(bookId);
+    Book.removeBook(bookId);
   }
 });
